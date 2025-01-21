6352db8dcc23029991721694218ed60889d28779
Sayandeep <sdroy001@gmail.com>
2025-01-21 23:42:45 +0530

    3rd change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,8 +32,7 @@ const About = () => {
           Welcome to Zentry
         </h2>
 
-        <AnimatedTitle title="Disc<b>o</b>ver the world's <br/>
-         largest shared <b>a</b>dventure" containerClass="mt-5 !text-black text-center"/>
+        <AnimatedTitle title="Disc<b>o</b>ver the world's<br/>largest shared <b>a</b>dventure" containerClass="mt-5 !text-black text-center"/>
 
 
         <div className="about-subtext">
